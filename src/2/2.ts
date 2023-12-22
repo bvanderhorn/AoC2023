6b2eda4c52d57f55d4b8777e05d30f7e3e5af5b3
bvanderhorn <bvanderhorn@gmail.com>
2023-12-22 13:01:18 +0100

    feat: day 2 init

---
+++ src/2/2.ts
@@ -0,0 +1,26 @@
+import * as h from '../helpers';
+
+type rgb = [number, number, number];
+type game = {
+    id: number,
+    grabs: rgb[]
+}
+
+var getGame = (input: string) : game => {
+    var [id_str, grabs_str] = input.split(':');
+    var id = +(id_str.replace(/[^0-9]/g,""));
+    var grabs_strs = grabs_str.split(';');
+    var grabs = grabs_strs.map(x => 
+        x.split(',').map(y => {
+            var val = +(y.replace(/[^0-9]/g,""));
+            return [y.includes('red') ? val : 0, y.includes('green') ? val : 0, y.includes('blue') ? val : 0];
+        }).sum0() as rgb
+    );
+    return {id, grabs};
+}
+
+var games = h.read("2", "games.txt").map(g => getGame(g));
+var rgbMax: rgb = [12, 13, 14];
+
+h.print(games[0]);
+
