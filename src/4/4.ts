5f88a8e35e0f3edb5f5ed02be575b04463419c2b
bvanderhorn <bvanderhorn@gmail.com>
2023-12-23 10:47:55 +0100

    feat: day 4 pt 2

--- src/4/4.ts
+++ src/4/4.ts
@@ -4,14 +4,29 @@ class Card {
     public id: number;
     public winning: number[];
     public actual: number[];
+    public matching : number;
     constructor(cardString: string) {
-        var matches = cardString.match(/^Card\s+([0-9]+):([\s0-9]+)\|(.*)$/)!.slice(1);
-        this.id = +matches[0];
-        this.winning = matches[1].trim().split(/\s+/).map(x => +x); 
-        this.actual = matches[2].trim().split(/\s+/).map(x => +x);
+        var matches = cardString.match(/^Card\s+([0-9]+):([\s0-9]+)\|(.*)$/)!;
+        this.id = +matches[1];
+        this.winning = matches[2].trim().split(/\s+/).tonum(); 
+        this.actual = matches[3].trim().split(/\s+/).tonum();
+        this.matching = this.winning.intersect(this.actual).length;
     }
 }
 
 var cards = h.read("4", "cards.txt").map(x => new Card(x));
 h.print(cards[0]);
-h.print('part 1:', cards.map(c => c.winning.intersect(c.actual).length).map(x => x == 0 ? 0 : 2**(x-1)).sum());
+h.print('part 1:', cards.map(c => c.matching).map(x => x == 0 ? 0 : 2**(x-1)).sum());
+
+// part 2
+var copies = h.ea(cards.length, 1);
+for (var i = 0; i < cards.length; i++) {
+    var matching = cards[i].matching;
+    for (var j = 0; j<matching; j++) {
+        var k = i + j + 1;
+        if (k >= copies.length)
+            continue;
+        copies[k] += copies[i];
+    }
+}
+h.print("part 2:",copies.sum());
