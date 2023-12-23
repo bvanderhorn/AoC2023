779b650f96c9bd0b1f7b86f8e0b1f6585a44456a
bvanderhorn <bvanderhorn@gmail.com>
2023-12-23 13:38:06 +0100

    feat: day 5 pt 1

--- src/5/5.ts
+++ src/5/5.ts
@@ -11,11 +11,30 @@ class AMap {
         this.to = from_to[2];
         this.map = input.slice(1).split(' ').tonum();
     }
+
+    public convert(amount:number) : number {
+        for (const [tostart, fromstart, length] of this.map) {
+            if (amount >= fromstart && amount < (fromstart + length)) {
+                var delta = amount - fromstart;
+                return tostart + delta;
+            }
+        }
+        return amount;
+    }
 }
 
 var almanac = h.read("5", "almanac.txt");
-var seeds = almanac[0][0].split(' ').slice(1).tonum();
+var seeds: number[] = almanac[0][0].split(' ').slice(1).tonum();
 var maps = almanac.slice(1).map(x => new AMap(x));
 
 h.print(seeds);
 h.print(maps[0]);
+
+var type = 'seed';
+while (type != 'location') {
+    var map = maps.find(m => m.from == type)!;
+    seeds = seeds.map(s => map.convert(s));
+    type = map.to;
+}
+
+h.print("part 1:", seeds.min());
