5c909470d3d98a0050a553eee22b78c7efb37a6e
bvanderhorn <bvanderhorn@gmail.com>
2023-12-27 10:26:37 +0100

    feat: day 6 pt 2

--- src/6/6.ts
+++ src/6/6.ts
@@ -1,3 +1,4 @@
+import { count } from 'console';
 import * as h from '../helpers';
 
 var getDistances = (time:number) : number[] => h.range(0,time).map(x => x * (time - x));
@@ -6,3 +7,6 @@ var countDistancesOver = (time: number, distance: number) : number => getDistanc
 var races = h.read("6", "races.txt").split(/\s+/).map(x => x.slice(1).tonum());
 h.print(races);
 h.print("part 1:", h.range(0, races[0].length).map(i => countDistancesOver(races[0][i], races[1][i])).prod());
+var race = races.map(x => +x.join(""));
+h.print(race);
+h.print("part 2:", countDistancesOver(race[0], race[1]));
