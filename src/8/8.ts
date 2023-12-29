800994e9f7c6b65c40bef61694246cf2a5a6b51e
bvanderhorn <bvanderhorn@gmail.com>
2023-12-29 10:33:06 +0100

    feat: day 8 pt 1

--- src/8/8.ts
+++ src/8/8.ts
@@ -10,7 +10,21 @@ class Node {
     }
 }
 
-var [instructions, rawNodes] = h.read("8", "maps.txt", "ex");
-var nodes = rawNodes.map((x:string) => new Node(x)).map((x:Node) => [x.name, x]).todict();
+var applyStep = (current: Node, instruction:string) : Node => nodes.get(instruction[0] == "L" ? current.left : current.right)!;
 
-h.print(nodes);
+var [instructions, rawNodes] = h.read("8", "maps.txt");
+var nodes = rawNodes.map((x:string) => new Node(x)).map((x:Node) => [x.name, x]).todict();
+instructions = instructions[0];
+
+var start = "AAA";
+var end = "ZZZ";
+
+var current = nodes.get(start);
+var steps = 0;
+while (current.name != end) {
+    var instruction = instructions.get(steps);
+    // h.print(current.name,'-', instruction,'=>',applyStep(current, instruction).name);
+    current = applyStep(current, instructions.get(steps));
+    steps++;
+}
+h.print("part 1:", steps);
