f5386fad54dc26c894506a2008ac6c366914c984
bvanderhorn <bvanderhorn@gmail.com>
2023-12-30 13:53:59 +0100

    feat: day 9 pt 2

--- src/9/9.ts
+++ src/9/9.ts
@@ -1,15 +1,28 @@
 import * as h from '../helpers';
 
+var getDiffs = (input: number[]) : number[] => h.range(0, input.length-1).map(i => input[i+1] - input[i]);
+
 var findNext = (history: number[]) : number => {
-    var add = history.last();
-    var diffs = history.copy();
+    var [diffs, add] = [history.copy(), 0];
     while (true) {
-        diffs = h.range(0, diffs.length-1).map(i => diffs[i+1] - diffs[i]);
+        add += diffs.last();
+        diffs = getDiffs(diffs);
         if (diffs.filter(x => x!=0).length == 0)
             return add;
-        add += diffs.last();
+    }
+}
+
+var findPrev = (history: number[]) : number => {
+    var [diffs, add, iterator] = [history.copy(), 0, 1];
+    while(true) {
+        add += iterator*diffs[0];
+        iterator *= -1;
+        diffs = getDiffs(diffs);
+        if (diffs.filter(x => x!=0).length == 0)
+            return add;
     }
 }
 
 var histories = h.read("9", "histories.txt").split(' ').tonum();
 h.print("part 1:", histories.map(x => findNext(x)).sum());
+h.print("part 2:", histories.map(x => findPrev(x)).sum());
