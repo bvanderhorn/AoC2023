88ae8cc9bbcc5154c3cca14b03756968c66a5318
bvanderhorn <bvanderhorn@gmail.com>
2024-01-03 13:41:25 +0100

    feat: progress

--- src/12/12.ts
+++ src/12/12.ts
@@ -1,11 +1,28 @@
 import * as h from '../helpers';
+class Mask {
+	public trues: number[];
+	public falses: number[];
+	public length : number;
+	constructor(maskString: string) {
+		this.trues = h.findAllOccurrencesOf(/#/g, maskString).map(x => x.index);
+		this.falses = h.findAllOccurrencesOf(/\./g, maskString).map(x => x.index);
+		this.length = maskString.length;
+	}
+}
+class Spring  {
+	public mask: Mask;
+	public struct: number[]
+	constructor(springString: string) {
+		var [maskString, structString] = springString.split(" ");
+		this.mask = new Mask(maskString);
+		this.struct = structString.split(',').tonum();
+	}
+}
 
-var springs = h.read("12", "springs.txt").split(' ');
+var getSolutions = (spring: Spring) : boolean[][] => {
+	var solutions: boolean[][] = [];
+}
+
+var springs: Spring[] = h.read("12", "springs.txt").map(s => new Spring(s));
 
 h.print(springs.slice(0,2));
-
-var bruteSolve = (spring: string[]) : number {
-	var field = spring[0];
-	var values: number[] = spring[1].split(',').tonum();
-
-}
