fce4d8902b391df8f38bba0639d3419d27b57f89
bvanderhorn <bvanderhorn@gmail.com>
2024-01-10 10:51:32 +0100

    feat: progress

--- src/17/17a.ts
+++ src/17/17a.ts
@@ -3,7 +3,6 @@ import * as h from '../helpers';
 type Coor = [number, number];
 class Node {
     public value: [number, number, string, number]; // [distance from init, potential, direction, location integer]
-    public from: Node | undefined; // will be undefined for init
     public loc: number; // location integer
     public id: string; // unique id
 
@@ -11,7 +10,8 @@ class Node {
         public dist: number, // distance from init
         public pot: number, // potential (number of times the direction has been this before and including this time; max 3)
         public dir: string, // direction, one of 'udlr'
-        public coor: Coor  // coordinates [x, y]
+        public coor: Coor,  // coordinates [x, y]
+        public from: Node | undefined = undefined // will be undefined for init
         ) {
         this.loc = coorToInt(coor);
         this.value = [dist, pot, dir, this.loc];
@@ -39,7 +39,7 @@ var setToNext = (n: Node, next: [number, Node[]][]) : void => {
     else next[index][1].push(n);
 }
 
-var bmap = h.read("17", "map.txt").split('').tonum();
+var bmap = h.read("17", "map.txt", "ex").split('').tonum();
 
 // Fast Dijkstra (because using Maps) with weighted distances
 // input
@@ -54,6 +54,7 @@ var next2 = new Map<string, Node>(); // id => Node
 var visited = new Map<string, Node>(); // id => Node
 
 // find all distances from init for all nodes
+var goalN: Node | undefined = undefined;
 while(next.length > 0){
     // get lowest set of nexts as curs
     next.sort((n1, n2) => n1[0] - n2[0]);
@@ -69,10 +70,10 @@ while(next.length > 0){
         var nb: Node[] = allNbCoor.map(c => {
             var dir = getDir(cur.coor, c);
             var pot = cur.dir == dir ? cur.pot + 1 : 1;
-            return new Node(dist + bmap[c[0]][c[1]], pot, dir, c);
+            return new Node(dist + bmap[c[0]][c[1]], pot, dir, c, cur);
         });
         // filter on pot <= 3 and unvisited
-        nb = nb.filter(n => n.pot <= 3 && !visited.has(n.id));
+        nb = nb.filter(n => n.pot <= 3 && !visited.has(n.id)); // <= implementation of max-3-straight-steps rule!!
 
         // add to next or update if distance shorter than current
         for (const n of nb) {
@@ -96,20 +97,27 @@ while(next.length > 0){
         // add cur to visited
         visited.set(cur.id, cur);
         next2.delete(cur.id);
+
+        // check if goal
+        if (cur.loc == coorToInt(goal)) {
+            goalN = cur;
+            break;
+        }
     }
+
+    // break if goal
+    if (goalN != undefined) break;
 }
 
 // get shortest path and dist from init to goal
-var goalInt = coorToInt(goal);
-var dist = visited.get(goalInt)![1];
-var intPath = [goalInt];
-var cur = goalInt;
-while(cur != coorToInt(start)){
-    var [fromNb, _] = visited.get(cur)!;
-    intPath.unshift(fromNb);
-    cur = fromNb;
+if (goalN == undefined) throw "no path found";
+var path: Node[] = [goalN];
+var cur = goalN;
+while(cur.id != startN.id){
+    path.unshift(cur.from!);
+    cur = cur.from!;
 }
 
 // print shortest path
-h.print("shortest path from", start, "to", goal, "is", dist, "long");
-bmap.printc((_, i, j) => intPath.includes(coorToInt([i,j])), 'm');
+h.print("shortest path from", start, "to", goal, "is", goalN.dist, "long");
+bmap.printc((_, i, j) => path.map(p=>p.loc).includes(coorToInt([i,j])), 'm');
