288f93d5c2c7855a6955ddf8a4f79405adb0af55
bvanderhorn <bvanderhorn@gmail.com>
2024-01-13 10:26:30 +0100

    feat: progress

--- src/18/18.ts
+++ src/18/18.ts
@@ -11,24 +11,19 @@ var newCoor = (coor: Coor, dig: Dig) : Coor => {
         [x, y];
 }
 
-var digplan = h.read("18", "digplan.txt")
+var digplan = h.read("18", "digplan.txt", "ex")
     .match(/(\w)\s+(\d+)\s+\((#[\d\w]+)\)/)
     .map(d => ({dir: d[0].toLowerCase(), dist: +d[1], hex: d[2]}));
 h.print(digplan.slice(0,3));
 
+// TODO: merge subsequent dig actions that are in the same direction
+
 var start : Coor = [0,0];
 var nodes : Coor[] = [start];
 digplan.map(d => nodes.push(newCoor(nodes.last(), d)));
-var path = h.expandTrace(nodes);
-var pathx = path.map(p => p[0]);
-var pathy = path.map(p => p[1]);
 
-h.print(pathx.min(), pathx.max(), pathy.min(), pathy.max());
-
-//h.print(nodes);
-//h.print("prepping the map..");
-//var digmap = h.coorToMap(nodes.map(p => [p[0], p[1], 1]), (x) => x==1 ? '#' : '.');
-//h.print("printing the map...");
-//digmap.printc((x) => x == '#', 'm');
-
-h.print("part 1:",h.getSnakeInternalFields(null, path as [number,number][], true, true).length);
+var evens = 'ud'.includes(digplan[0].dir);
+var verticals: [Coor, Coor][] = [];
+nodes.map((n,i) => (i%2==0 && evens || i%2==1 && !evens) ? verticals.push(nodes.slice2(i, i+2) as [Coor, Coor]) : null);
+h.print(verticals);
+// h.print("part 1:",h.getSnakeInternalFields(null, path as [number,number][], true, true).length);
