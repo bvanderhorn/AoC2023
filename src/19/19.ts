e473bb99bf667b4dfff5d390147bcc4f26055fe0
bvanderhorn <bvanderhorn@gmail.com>
2024-01-16 11:11:22 +0100

    feat: day 19 pt 1

--- src/19/19.ts
+++ src/19/19.ts
@@ -1,11 +1,10 @@
 import * as h from '../helpers';
 
 type Condition = [string, string, number];
-type Check = {to: string, condition: Condition|undefined}
-type Rule = {id:string, checks: Check[]};
+type Check = {to: string, condition: Condition|undefined};
 type Part = {x:number, m:number, a:number, s:number};
 
-var toRule = (rawRule: string) : Rule => {
+var toRule = (rawRule: string) : [string, Check[]] => {
     var [id, rawChecks] = rawRule.match(/(\w+)\{([^\}]+)\}/)!.slice(1);
     const checks : Check[] = rawChecks.split(',').map((rawCheck) => {
         const parts = rawCheck.split(':');
@@ -14,12 +13,35 @@ var toRule = (rawRule: string) : Rule => {
         const condition = rawCondition.match(/(\w)([<>=])(\d+)/)!.slice(1);
         return {to, condition: [condition[0], condition[1], +condition[2]]};
     });
-    return {id, checks};
+    return [id, checks];
+}
+var value = (part: Part) : number => part.x + part.m + part.a + part.s;
+
+var checkPart = (part: Part) : boolean => {
+    var curChecks = rules.get("in");
+    while(true) {
+        for (const check of curChecks) {
+            var goto = false;
+            if (check.condition === undefined) goto = true;
+            if (!goto) {
+                var [x, op, m] = check.condition;
+                var val = x == 'x' ? part.x : x == 'm' ? part.m : x == 'a' ? part.a : part.s;
+                goto = (op == '<' && val < m || op == '>' && val > m || op == '=' && val == m);
+            }            
+            
+            if (goto) {
+                var next = check.to;
+                if (["A", "R"].includes(next)) return next == "A";
+                curChecks = rules.get(next);
+                break;
+            }
+        }
+    }
 }
 
-const [rawRules, rawParts] = h.read("19", "rulesparts.txt", "ex");
+const [rawRules, rawParts] = h.read("19", "rulesparts.txt");
 
-const rules = rawRules.map(toRule);
-h.print(h.stringify(rules[0]));
+const rules = rawRules.map(toRule).todict();
+h.print(rules.get("in"));
 const parts : Part[] = rawParts.map((p:string) => eval('p = ' + p.replace(/=/g, ':')));
-h.print(parts);
+h.print("part 1:", parts.filter(checkPart).map(value).sum());
