8d4c12dd08130f095e0ccbb52710d3359fbded1b
bvanderhorn <bvanderhorn@gmail.com>
2024-01-23 13:15:01 +0100

    feat: progress

--- src/20/20.ts
+++ src/20/20.ts
@@ -15,15 +15,15 @@ type Pulse = {
 
 var printPulse = (p: Pulse) => (p.hl ? "high" : "low") + " => " + p.to.id;
 
-var applyCycle = (modules: Map<string, Module>, v:boolean = false) : [number, number, boolean] => {
+var applyCycle = (modules: Map<string, Module>, v:boolean = false, v2:boolean = false) : [number, number, boolean] => {
     var highlow : [number, number] = [0, 0];
     var remaining : Pulse[] = [{hl:false, to: modules.get("broadcaster")!}];
-    var countLowRx = 0;
+    var highLowRx: [number, number] = [0, 0];
     while (remaining.length > 0) {
         var pulse = remaining.shift()!;
         h.printv(v, "pulse:", printPulse(pulse));
-        if (!pulse.hl && pulse.to.id == 'rx') countLowRx++;
-        highlow[pulse.hl ? 1 : 0]++;
+        if (pulse.to.id == 'rx') highLowRx[pulse.hl ? 0 : 1]++;
+        highlow[pulse.hl ? 0 : 1]++;
         var module = pulse.to;
         var toPush: Pulse[] = [];
         if (!"%&".includes(module.type)) toPush = module.to.map(m => ({hl: pulse.hl, to: m}));
@@ -40,7 +40,8 @@ var applyCycle = (modules: Map<string, Module>, v:boolean = false) : [number, nu
         remaining.push(...toPush);
     }
     h.printv(v,"cycle:", highlow);
-    return [highlow[0], highlow[1], countLowRx == 1];
+    h.printv(v2, "cycle", cycles, ", highLowRx:", highLowRx);
+    return [highlow[0], highlow[1], highLowRx[1] == 1];
 }
 
 var modules: Map<string, Module> = h.read("20", "modules.txt").map(x => {
@@ -67,5 +68,5 @@ h.print("part 1:", highlow.prod());
 // part 2
 modules.forEach(m => m.state = false);
 var cycles = 0;
-while (++cycles) if (applyCycle(modules)[2]) break;
+while (++cycles) if (applyCycle(modules, false, false)[2]) break;
 h.print("part 2:", cycles);
