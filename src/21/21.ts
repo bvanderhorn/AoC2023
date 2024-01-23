8b1599528d5435b67a1105781aef76e20aa43ad1
bvanderhorn <bvanderhorn@gmail.com>
2024-01-23 16:04:08 +0100

    feat: progress

--- src/21/21.ts
+++ src/21/21.ts
@@ -3,20 +3,16 @@ type Coor = [number, number];
 var toId = (location: Coor) : number => location[0]*garden[0].length + location[1];
 var fromId = (id: number) : Coor => [Math.floor(id/garden[0].length), id%garden[0].length];
 var getLocalNb = (location: number) : number[] => {
-    const nbs : number[] = [];
-    if (location % garden[0].length > 0) nbs.push(location-1);
-    if (location % garden[0].length < garden[0].length-1) nbs.push(location+1);
-    nbs.push(location-garden[0].length);
-    nbs.push(location+garden[0].length);
-
-    return nbs.filter(x => x >= 0 && x < garden.length*garden[0].length);
+    var loc = fromId(location);
+    return ([[loc[0]-1, loc[1]], [loc[0]+1, loc[1]], [loc[0], loc[1]-1], [loc[0], loc[1]+1]] as Coor[])
+        .filter(x => x[0] >= 0 && x[0] < garden.length && x[1] >= 0 && x[1] < garden[0].length).map(toId);
 }
-var getnb = (location: number) : number[] => getLocalNb(location).filter(id => !rocks.includes(id));
+var getnb = (location: number) : number[] => getLocalNb(location).filter(id => !rocks.has(id));
 
 const garden = h.read("21", "garden.txt").split('');
 
 garden.stringc(x => x === "#", 'r', '','\n', 20).printc(x => x === "S", "c");
-const rocks = garden.getCoors(x => x === "#")!.map(x => toId(x as Coor));
+const rocks = (garden.getCoors(x => x === "#")! as Coor[]).map(toId).toSet();
 const start = toId(garden.getCoor(x => x === "S")! as Coor);
 
 // part 1
