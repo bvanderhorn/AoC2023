c516ec2d01800a42d1f4c9e31be0a61fd64e5448
bvanderhorn <bvanderhorn@gmail.com>
2024-01-23 21:28:49 +0100

    feat: progress

--- src/21/21a.ts
+++ src/21/21a.ts
@@ -6,32 +6,33 @@ var reduce = (loc: number[]) : Coor => [(loc[0]%garden.length + garden.length)%g
 var isRock = (loc: number[]) : boolean => rocks.has(reduce(loc));
 var getnb = (loc: number[]) : Coor[] => getLocalNb(loc).filter(x => !isRock(x)) as Coor[];
 var calculate = (maxCount: number, part:number = 1) : number => {
-    var current = new h.DoubleSet<number>();
-    current.add(start);
+    var current = new h.DoubleSet<number>([start]);
     var last = new h.DoubleSet<number>();
-    var counter = -1;
+    var counter = 0;
     var [evens, odds] = [1, 0];
     var v = false; // verbose
     console.time("part "+ part);
-    while (++counter < maxCount) {
+    while (++counter <= maxCount) {
         //h.printv(v, "i:", counter, "last:", [...last.keys()], "current:", [...current.keys()]);
         var newCurrent = new h.DoubleSet<number>();
         current.forEach(loc => getnb(loc).forEach(nb => {
             if (!last.has(nb)) newCurrent.add(nb);
         }));
-        last = current;
-        current = newCurrent;
-        if (counter%2 == 0) odds += newCurrent.size; else evens += newCurrent.size;
+        [last, current] = [current, newCurrent];
+        if (counter%2 == 0) evens += newCurrent.size; else odds += newCurrent.size;
         //h.printv(v,"evens:", evens, "odds:", odds);
     }
     console.timeEnd("part " + part);
+    printCurrent(current, last);
     return maxCount%2 == 0 ? evens : odds;
 }
+var printCurrent = (current: h.DoubleSet<number>, last:h.DoubleSet<number>) : void => 
+    garden.mapij((i,j,_) => current.has([i,j]) ? "O" : last.has([i,j]) ? "@" : garden[i][j]).stringc(x => x === "@", "c").printc(x => x == "O", "r");
 
 const garden = h.read("21", "garden.txt").split('');
 // garden.stringc(x => x === "#", 'r', '','\n', 15).printc(x => x === "S", "c");
 const rocks = new h.DoubleSet<number>(garden.getCoors(x => x === "#")! as Coor[]);
 const start = garden.getCoor(x => x === "S")! as Coor;
 
-h.print("part 1:", calculate(64, 1));
-h.print("part 2:", calculate(1E4, 2));
+h.print("part 1:", calculate(65, 1));
+// h.print("part 2:", calculate(1E3, 2));
