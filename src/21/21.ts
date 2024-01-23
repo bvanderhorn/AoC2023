f43db9334ff2674c2bbd316a73cf6b40c75c7779
bvanderhorn <bvanderhorn@gmail.com>
2024-01-23 15:44:11 +0100

    feat: day 21 pt 1

--- src/21/21.ts
+++ src/21/21.ts
@@ -1,5 +1,21 @@
 import * as h from '../helpers';
+type Coor = [number, number];
+var toId = (location: Coor) : number => location[0]*garden[0].length + location[1];
+var fromId = (id: number) : Coor => [Math.floor(id/garden[0].length), id%garden[0].length];
+var getnb = (location: number) : number[] => h.getnb(fromId(location), garden.length, garden[0].length)
+    .map(x => toId(x as Coor))
+    .filter(id => !rocks.includes(id));
 
-const garden = h.read("21", "garden.txt","ex").split('');
+const garden = h.read("21", "garden.txt").split('');
 
 garden.stringc(x => x === "#", 'r', '','\n', 30).printc(x => x === "S", "c");
+const rocks = garden.getCoors(x => x === "#")!.map(x => toId(x as Coor));
+const start = toId(garden.getCoor(x => x === "S")! as Coor);
+
+var locations : Set<number> = [start].toSet();
+for (const _ of h.range(0,64)) {
+    var newLocations = new Set<number>();
+    locations.forEach(location => getnb(location).forEach(x => newLocations.add(x)));
+    locations = newLocations;
+}
+h.print("part 1:", locations.size);
