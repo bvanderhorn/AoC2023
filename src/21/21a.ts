e0908b158c26fc55e7a3a0ddd37f3bb416f23039
bvanderhorn <bvanderhorn@gmail.com>
2024-01-24 03:50:00 +0100

    feat: progress

--- src/21/21a.ts
+++ src/21/21a.ts
@@ -19,7 +19,7 @@ var calculate = (maxCount: number, part:number = 1) : number => {
             if (!last.has(nb)) newCurrent.add(nb);
         }));
         // if (counter == maxCount) print3(newCurrent, current, last);
-        // if (counter == maxCount) printCurrent(newCurrent);
+        //if (counter == maxCount) printCurrent(newCurrent);
         [last, current] = [current, newCurrent];
         if (counter%2 == 0) evens += newCurrent.size; else odds += newCurrent.size;
         //h.printv(v,"evens:", evens, "odds:", odds);
@@ -35,8 +35,8 @@ var square = (odd:boolean) : number => {
 }
 var corners = (odd:boolean) : number => {
     // return the odd or even fields inside the outer corners
-    var all = garden.mapij((i,j,_) => (i+j)%2 == (odd ? 1 : 0) && start.manhattan([i,j]).sum() > Math.ceil(garden.length/2) ? 1 : 0).sum0().sum();
-    return all - rocks.values().filter(r => (r[0]+r[1])%2 == (odd ? 1 : 0) && start.manhattan(r).sum() > Math.ceil(garden.length/2)).length;
+    var all = garden.mapij((i,j,_) => ((i+j)%2 == (odd ? 1 : 0) && start.manhattan([i,j]).sum() > Math.floor(garden.length/2)) ? 1 : 0).sum0().sum();
+    return all - rocks.values().filter(r => ((r[0]+r[1])%2 == (odd ? 1 : 0) && start.manhattan(r).sum() > Math.floor(garden.length/2))).length;
 }
 var print3 = (newCurrent: h.DoubleSet<number>, current: h.DoubleSet<number>, last:h.DoubleSet<number>) : void => 
     garden.mapij((i,j,_) => newCurrent.has([i,j]) ? "O" 
@@ -60,12 +60,12 @@ var printCurrent = (current: h.DoubleSet<number>) : void => {
         .printc(x => x == "O", "r");
 }
 
-const garden = h.read("21", "garden.txt").split('');
+const garden = h.read("21", "garden.txt", "ex3").split('');
 // garden.stringc(x => x === "#", 'r', '','\n', 15).printc(x => x === "S", "c");
 const rocks = new h.DoubleSet<number>(garden.getCoors(x => x === "#")! as Coor[]);
 const start = garden.getCoor(x => x === "S")! as Coor;
 
-h.print("part 1:", calculate(27, 1));
+h.print("part 1:", calculate(5, 1));
 
 // part 2
 h.print("(", 26501365, " - 65) % 131:", (26501365-65)%131);
@@ -80,7 +80,7 @@ var evenSign = m%2 == 0 ? 1 : -1;
 h.print("oddRoot:", oddRoot, "oddSign:", oddSign, "evenRoot:", evenRoot, "evenSign:", evenSign);
 var [sqOdd, sqEven, corOdd, corEven] = [square(true), square(false), corners(true), corners(false)];
 h.print("square, odd:", sqOdd, "even:", sqEven, "corners, odd:", corOdd, "even:", corEven);
-h.print(sqOdd + sqEven + rocks.size, " = 131^2:", 131**2);
-h.print(corOdd + corEven + rocks.values().filter(r => start.manhattan(r).sum() > 65).length, " = 131^2 - 65^2:", 131**2 - 65**2);
+h.print(sqOdd + sqEven + rocks.size, " = 131^2:", garden.length**2);
+h.print(corOdd + corEven + rocks.values().filter(r => start.manhattan(r).sum() > Math.floor(garden.length/2)).length, " = 0.5*(n-1)*(n+1) = 65*132:", 0.5*(garden.length-1)*(garden.length+1));
 
 h.print("part 2:", (oddRoot**2)*sqOdd + oddSign*oddRoot*corOdd + evenRoot**2*sqEven + evenSign*evenRoot*corEven);
