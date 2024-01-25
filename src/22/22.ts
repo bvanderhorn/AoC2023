a3848cae7b3cd4ca3fb340ceae3d0632fd6644de
bvanderhorn <bvanderhorn@gmail.com>
2024-01-25 13:04:11 +0100

    feat: progress

--- src/22/22.ts
+++ src/22/22.ts
@@ -1,24 +1,38 @@
 import * as h from '../helpers';
 type Coor = [number, number, number];
-type Brick = [Coor, Coor];
-var zmin = (b: Brick) : number => Math.min(b[0][2], b[1][2]);
-var overlaps = (b1: Brick, b2: Brick) : boolean => h.overlaps([b1[0][0],b1[1][0]], [b2[0][0],b2[1][0]]) 
-                                                && h.overlaps([b1[0][1],b1[1][1]], [b2[0][1],b2[1][1]]) 
-                                                && h.overlaps([b1[0][2],b1[1][2]], [b2[0][2],b2[1][2]]);
+class Brick {
+    public supportedBy: Brick[] = [];
+    constructor(public value: [Coor, Coor]) {
 
-var bricks : Brick[] = h.read("22", "bricks.txt", "ex").split("~").mape(x => x.split(",").tonum());
-bricks.sort((a,b) => zmin(a) - zmin(b));
+    }
+    public get zmin() : number {
+        return Math.min(this.value[0][2], this.value[1][2]);
+    }
+    public overlaps(b: Brick) : boolean {
+        return h.overlaps([this.value[0][0],this.value[1][0]], [b.value[0][0],b.value[1][0]]) 
+            && h.overlaps([this.value[0][1],this.value[1][1]], [b.value[0][1],b.value[1][1]]) 
+            && h.overlaps([this.value[0][2],this.value[1][2]], [b.value[0][2],b.value[1][2]]);
+    }
+
+    public drop(dz:number) : Brick {
+        return new Brick(this.value.map(c => c.plusEach([0,0,-dz])) as [Coor, Coor]);
+    }
+    
+}
+
+var bricks : Brick[] = h.read("22", "bricks.txt", "ex").split("~").mape(x => new Brick(x.split(",").tonum()));
+bricks.sort((a,b) => a.zmin - b.zmin);
 h.print(bricks.slice(0,3));
 
 // drop the bricks
 var droppedBricks: Brick[] = [];
 for (const b of bricks){
-    for (var i = 0; i < zmin(b); i++) {
-        var testBrick : Brick = b.map(c => c.plusEach([0,0,-i])) as Brick;
-        var newBrick : Brick = b.map(c => c.plusEach([0,0,-i+1])) as Brick;
-        if (droppedBricks.some(db => overlaps(db, testBrick))) {
+    for (var i = 0; i < b.zmin; i++) {
+        var testBrick : Brick = b.drop(i);
+        var newBrick : Brick = b.drop(i-1);
+        if (droppedBricks.some(db => db.overlaps(testBrick))) {
             droppedBricks.push(newBrick);
             break;
-        } else if (i == zmin(b)-1) droppedBricks.push(testBrick);
+        } else if (i == b.zmin-1) droppedBricks.push(testBrick);
     }
 }
