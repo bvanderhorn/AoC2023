640fdfb796d033e9c6fc254fbdaadb59d59e5a59
bvanderhorn <bvanderhorn@gmail.com>
2024-02-04 15:53:53 +0100

    feat: progress

--- src/24/24.ts
+++ src/24/24.ts
@@ -1,14 +1,15 @@
 import * as h from '../helpers';
-type Coor = [number, number, number];
-type Stone = [Coor, Coor];
+type Coor3 = [number, number, number];
+type Stone3 = [Coor3, Coor3];
+type Coor2 = [number, number];
+type Stone2 = [Coor2, Coor2];
 
-var toLinearXY = (s: Stone) : [number, number] => {
-    var [[px, py, _], [vx, vy, __]] = s;
+var toLinearXY = (s: Stone2) : [number, number] => {
+    var [[px, py], [vx, vy]] = s;
     var dydx = vx == 0 ? vy > 0 ? Infinity : -Infinity : vy/vx;
     return [dydx, py - px * vy/vx];
 }
 
-var intersectXY = (a: Stone, b: Stone) : => {
-
-var stones : Stone[] = h.read("24", "stones.txt", "ex").split(" @ ").split(", ").tonum();
-h.print(stones.slice(0,3));
+var stones : Stone3[] = h.read("24", "stones.txt", "ex").split(" @ ").split(", ").tonum();
+var stones2 : Stone2[] = stones.map(s => s.map(c => c.slice(0,2)) as Stone2);
+h.print(stones2.slice(0,3));
